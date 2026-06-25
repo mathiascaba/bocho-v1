6327f91aa67f574c63f43b4c794298c6e28f3419
Mathias <mathias@example.com>
2026-06-25 01:46:12 -0500

    Dump env vars

--- functions/test.js
+++ functions/test.js
@@ -1,14 +1,5 @@
 export default async (req, context) => {
-  try {
-    const store = context.blobs
-    await store.setJSON('test:hello', { msg: 'world' })
-    const val = await store.get('test:hello', { type: 'json' })
-    return new Response(JSON.stringify({ success: true, value: val }), {
-      headers: { 'Content-Type': 'application/json' }
-    })
-  } catch (e) {
-    return new Response(JSON.stringify({ error: e.message, name: e.name, hasBlobs: !!context.blobs }), {
-      headers: { 'Content-Type': 'application/json' }
-    })
-  }
+  return new Response(JSON.stringify({ env: Object.keys(process.env).sort() }), {
+    headers: { 'Content-Type': 'application/json' }
+  })
 }
