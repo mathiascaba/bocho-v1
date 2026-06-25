b4316448fdf3b9e8f41d81d7684c876d1e71aee0
Mathias <mathias@example.com>
2026-06-25 01:49:18 -0500

    Check BLOBS_CONTEXT value

--- functions/test.js
+++ functions/test.js
@@ -1,23 +1,7 @@
-import { getStore } from '@netlify/blobs'
-
-exports.handler = async (event, context) => {
-  try {
-    const store = getStore('bcp-activaciones', {
-      siteID: process.env.SITE_ID,
-      token: process.env.NETLIFY_FUNCTIONS_TOKEN
-    })
-    await store.setJSON('test:hello', { msg: 'world' })
-    const val = await store.get('test:hello', { type: 'json' })
-    return {
-      statusCode: 200,
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ success: true, value: val })
-    }
-  } catch (e) {
-    return {
-      statusCode: 200,
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ error: e.message, name: e.name, code: e.code })
-    }
+exports.handler = async () => {
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ BLOBS_CONTEXT: process.env.NETLIFY_BLOBS_CONTEXT })
   }
 }
